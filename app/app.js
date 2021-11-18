29779da9b19c8f9890aa735022274428d519b479
febonatto <febonatto_1999@hotmail.com>
2021-11-18 19:37:43 -0300

    Finalizado

--- app/app.js
+++ app/app.js
@@ -6,11 +6,12 @@ const ammount = document.querySelector("#ammount span");
 const counter = document.querySelector("#button-container .counter");
 
 // variáveis
+let word = [];
 let ammountMoney = [0];
+let candySelected = "";
 let candyValue = 0;
 let candyQuantity = 0;
 let moneyRest = 0;
-let timeoutButton;
 
 // adição de evento as tags
 button.addEventListener("click", () => triggerButton());
@@ -19,83 +20,95 @@ candy.forEach((c) => c.addEventListener("click", () => triggerCandy(c)));
 
 // funções
 triggerButton = () => {
-	// usa o método que retorna qual das "candy" está selecionada
-	const selectedCandy = getSelectedCandy();
-
-	// define a cor da "candy" selecionada para geração da resposta
-	let color;
-	if (selectedCandy) {
-		if (selectedCandy.innerHTML === "A") color = "blue";
-		if (selectedCandy.innerHTML === "B") color = "yellow";
-		if (selectedCandy.innerHTML === "C") color = "green";
-	}
-
 	// realiza a animação e tira o poder de click do botão (prevenção de clicks rápidos)
 	button.style.animation = "animate-button 1000ms ease";
 	button.style.pointerEvents = "none";
-	// após 1s retira a animação do botão
+
+	// após 1s retira a animação do botão, retorna o click e da a resposta de suas escolhas para o usuário
 	setTimeout(() => {
 		button.style.animation = "";
+		button.style.pointerEvents = "all";
+
+		if (getSelectedCandy()) {
+			// coloca como final do alfabeto a "candy" escolhida
+			putAtWord(getSelectedCandy().id);
+
+			// realiza o escopo apenas se o total inserido for maior do que o valor da "candy"
+			if (getAmmountTotal() >= candyValue) {
+				// define o troco que será devolvido
+				moneyRest = getAmmountTotal() - candyValue;
+
+				// abre um alert informando o status das escolhas do usuário
+				alert(
+					`Você resgatou 1 doce ${candySelected}, ${
+						moneyRest
+							? "obteve R$" + moneyRest + ",00 de troco"
+							: "não tem troco"
+					} e parou no estado ${getFinalState()}`
+				);
+				
+				// exibe no console o alfabeto enviado pelo usuário
+				console.log(word);
+
+				// retorna as variáveis para o valor inicial e desseleciona a "candy"
+				resetProgram();
+			} else {
+				alert("Você não tem dinheiro para resgatar nenhum doce");
+			}
+		} else {
+			alert("Você precisa selecionar um doce para continuar");
+		}
 	}, 1000);
-	// caso a cor e a quantidade de "candy" estejam setadas ele retorna o click do botão
-	if (color && candyQuantity) {
-		ammount.innerHTML = `R$${moneyRest},00`;
-
-		timeoutButton = setTimeout(() => {
-			button.style.pointerEvents = "all";
-		}, 1000);
-
-		ammountMoney = [moneyRest];
-
-		calculate();
-	}
 };
 
 triggerMoney = (m) => {
+	// adiciona o valor clicado a palavra do AFD
+	putAtWord(m.id);
+
 	// adiciona o valor clicado ao array "ammountMoney"
-	ammountMoney.push(parseInt(m.innerHTML));
-	// cria a constante "ammountTotal" que vai receber a quantia total adicionada pelo usuário
-	const ammountTotal = getAmmountTotal();
-	// exibe esse total na tela para que o usuário possa enxergar
-	ammount.innerHTML = `R$${ammountTotal},00`;
+	ammountMoney.push(parseInt(m.id));
 
-	// percorre as minhas tags com a classe "money" e retira a ação de click delas
-	money.forEach((m) => (m.style.pointerEvents = "none"));
-	// após 1.5s percorre novamente e da a possibilidade de clicar de volta (prevenção de clicks rápidos)
-	setTimeout(() => {
-		money.forEach((m) => (m.style.pointerEvents = "all"));
-	}, 1500);
+	// exibe o total de dinheiro inserido na tela para que o usuário possa enxergar
+	ammount.innerHTML = `R$${getAmmountTotal()},00`;
 
-	calculate();
+	if (getSelectedCandy() && getAmmountTotal() >= candyValue) {
+		counter.innerHTML = 1;
+	}
 };
 
 triggerCandy = (c) => {
-	// percorre todas as tags com classe "candy" e remove a classe "grow" das que tiverem
-	candy.forEach((c) => {
-		c.classList.remove("selected");
-		c.style.animation = "";
-	});
-	// adiciona a classe "grow" apenas para a última clicada pelo usuário
+	// remove a classe "selected" da "candy" anteriormente selecionada
+	const selectedCandy = Array.from(candy).find((c) =>
+		c.classList.contains("selected")
+	);
+	if (selectedCandy) {
+		selectedCandy.classList.remove("selected");
+		selectedCandy.style.animation = "";
+	}
+
+	// adiciona a classe "selected" para a última "candy" clicada pelo usuário
 	c.classList.add("selected");
 	c.style.animation = "animate-candy 750ms linear infinite";
 
 	// acessa a variável local "candyValue" e atualiza o valor dependendo da "candy" escolhida pelo usuário
-	if (c.innerHTML === "A") candyValue = 6;
-	if (c.innerHTML === "B") candyValue = 7;
-	if (c.innerHTML === "C") candyValue = 8;
+	candySelected = c.id;
+	if (c.id === "A") candyValue = 6;
+	if (c.id === "B") candyValue = 7;
+	if (c.id === "C") candyValue = 8;
 
-	calculate();
+	if (getAmmountTotal() >= candyValue) {
+		counter.innerHTML = 1;
+	}
+};
+
+putAtWord = (character) => {
+	// adiciona o valor como string ao array de caracteres
+	word.push(character.toString());
 };
 
 getSelectedCandy = () => {
-	// cria a variável "selectedCandy" com o valor inicial null
-	let selectedCandy = null;
-	// percorre as tags com classe "candy" para ver se alguma esta selecionada
-	candy.forEach((c) => {
-		if (c.classList.contains("selected")) selectedCandy = c;
-	});
-	// retorna null caso nenhuma esteja selecionada ou retorna a "candy" selecionada
-	return selectedCandy;
+	// retorna a "candy" que possui "selected" na classe
+	return Array.from(candy).find((c) => c.classList.contains("selected"));
 };
 
 getAmmountTotal = () => {
@@ -103,21 +116,80 @@ getAmmountTotal = () => {
 	return ammountMoney.reduce((acc, curr) => (acc += curr));
 };
 
-calculate = () => {
-	// cria a constante "ammountTotal" que vai receber a quantia total adicionada pelo usuário
-	const ammountTotal = getAmmountTotal();
-	// acessa a variável local "candyQuantity" e atualiza seu valor para a quantidade de balas que podem ser compradas a partir da seleção
-	candyQuantity = Math.floor(ammountTotal / (candyValue || ammountTotal + 1));
-	// acessa a variável local "moneyRest" e atualiza seu valor de troco a partir da bala selecionada
-	moneyRest = ammountTotal % (candyValue || ammountTotal + 1);
-	// exibe na tela o total de balas que podem ser compradas
-	counter.innerHTML = candyQuantity;
+getFinalState = () => {
+	let state = "q0";
+	const numbers = word.filter((character) => !isNaN(parseInt(character)));
+	numbers.forEach((character) => {
+		const number = parseInt(character);
+		if (
+			(state === "q4" && number === 5) ||
+			(state === "q5" && number === 5) ||
+			(state === "q6" && number === 5) ||
+			(state === "q7" && number === 2) ||
+			(state === "q7" && number === 5) ||
+			(state === "q8" && !!number) ||
+			(state === "q9" && !!number)
+		) {
+			state = "q9";
+		} else {
+			if (state === "q0" && number === 1) state = "q1";
+			else if (state === "q0" && number === 2) state = "q2";
+			else if (state === "q0" && number === 5) state = "q5";
+			else if (state === "q1" && number === 1) state = "q2";
+			else if (state === "q1" && number === 2) state = "q3";
+			else if (state === "q1" && number === 5) state = "q6";
+			else if (state === "q2" && number === 1) state = "q3";
+			else if (state === "q2" && number === 2) state = "q4";
+			else if (state === "q2" && number === 5) state = "q7";
+			else if (state === "q3" && number === 1) state = "q4";
+			else if (state === "q3" && number === 2) state = "q5";
+			else if (state === "q3" && number === 5) state = "q8";
+			else if (state === "q4" && number === 1) state = "q5";
+			else if (state === "q4" && number === 2) state = "q6";
+			else if (state === "q5" && number === 1) state = "q6";
+			else if (state === "q5" && number === 2) state = "q7";
+			else if (state === "q6" && number === 1) state = "q7";
+			else if (state === "q6" && number === 2) state = "q8";
+			else if (state === "q7" && number === 1) state = "q8";
+		}
+	});
 
-	// caso a variável "candyQuantity" seja maior que 0 o botão de retirar se torna clicável, caso contrário não
-	if (candyQuantity > 0) {
-		button.style.pointerEvents = "all";
+	const candy = word[word.length - 1];
+	if (
+		(state === "q7" && candy === "A") ||
+		(state === "q8" && candy === "A") ||
+		(state === "q9" && candy === "A")
+	) {
+		state = "ACT";
+	} else if (
+		(state === "q8" && candy === "B") ||
+		(state === "q9" && candy === "B")
+	) {
+		state = "BCT";
 	} else {
-		button.style.pointerEvents = "none";
+		if (state === "q6" && candy === "A") state = "AST";
+		else if (state === "q7" && candy === "B") state = "BST";
+		else if (state === "q8" && candy === "C") state = "CST";
+		else if (state === "q9" && candy === "C") state = "CCT";
 	}
-	clearTimeout(timeoutButton);
+
+	return state;
+};
+
+resetProgram = () => {
+	word = [];
+	ammountMoney = [0];
+	candySelected = "";
+	candyValue = 0;
+	candyQuantity = 0;
+	moneyRest = 0;
+
+	const selectedCandy = Array.from(candy).find((c) =>
+		c.classList.contains("selected")
+	);
+	selectedCandy.classList.remove("selected");
+	selectedCandy.style.animation = "";
+
+	ammount.innerHTML = "R$0,00";
+	counter.innerHTML = 0;
 };
